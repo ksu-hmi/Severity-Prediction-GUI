3bb20633d35bf1af73bd8e6aefede1030268330c
Jana <jdunca86@students.kennesaw.edu>
2019-11-15 19:16:32 -0500

    Fixed Button
    will need to make reset button reset

--- _JD_GUI_Part.py
+++ _JD_GUI_Part.py
@@ -100,7 +100,7 @@ def clicked():
 tk.Button(r,text= "Quit",width= 15, command= r.quit).grid(row=3, column=0, sticky=tk.W, pady=4)
 tk.Button(r,text="Input", width= 15, command= show_entry_fields).grid(row=0, column=2, sticky=tk.W, pady=4)
 tk.Button(r, text= "Predict", width= 15,command= clicked).grid(row=3, column = 1, sticky=tk.W, pady=4)
-tk.Button(r, text= "Reset", width= 15,command= clicked).grid(row=3, column = 2, sticky=tk.W, pady=4)
+tk.Button(r, text= "Reset", width= 15,command= r.quit).grid(row=3, column = 2, sticky=tk.W, pady=4)
 
 
 
